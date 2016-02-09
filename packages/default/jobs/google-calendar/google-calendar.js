2907f7ceac82df95c1463a7a8c9628fafd00093b
josephsaffron <yellowedjoe>
2016-02-09 00:05:54 -0500

    Include weather and calendar data

--- packages/default/jobs/google-calendar/google-calendar.js
+++ packages/default/jobs/google-calendar/google-calendar.js
@@ -1,40 +1,128 @@
-var ical = require('ical');
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
 
-module.exports = {
+var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+  process.env.USERPROFILE) + '/.credentials/';
+  var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
 
-  onInit: function(config, dependencies) {
-    dependencies.logger.info('Google calendar job started!'); // I am a log line!
-  },
 
-  onRun: function (config, dependencies, jobCallback) {
-    var _ = dependencies.underscore;
-    var maxEntries = config.maxEntries || 10;
+  module.exports = {
 
-    var formatDate = function (date) {
-      var d = date.getDate();
-      var m = date.getMonth() + 1;
-      return '' + (m <= 9 ? '0' + m : m) + '/' + (d <= 9 ? '0' + d : d);
-    };
+    onInit: function(config, dependencies) {
+      dependencies.logger.info('Google calendar job started!'); // I am a log line!
+    },
 
-    ical.fromURL(config.calendarUrl, {}, function (err, data) {
+    onRun: function (config, dependencies, jobCallback) {
 
-      if (err) {
-        return jobCallback(err);
+      // Load client secrets from a local file.
+      fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+        if (err) {
+          jobCallback('Error loading client secret file: ' + err);
+        }
+        // Authorize a client with the loaded credentials, then call the
+        // Google Calendar API.
+        authorize(JSON.parse(content), listEvents);
+      });
+
+      /**
+      * Create an OAuth2 client with the given credentials, and then execute the
+      * given callback function.
+      *
+      * @param {Object} credentials The authorization client credentials.
+      * @param {function} callback The callback to call with the authorized client.
+      */
+      function authorize(credentials, callback) {
+        var clientSecret = credentials.installed.client_secret;
+        var clientId = credentials.installed.client_id;
+        var redirectUrl = credentials.installed.redirect_uris[0];
+        var auth = new googleAuth();
+        var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+        // Check if we have previously stored a token.
+        fs.readFile(TOKEN_PATH, function(err, token) {
+          if (err) {
+            getNewToken(oauth2Client, callback);
+          } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+          }
+        });
       }
 
-      var events = _.sortBy(data, function (event) {
-        return event.start;
-      });
-      events = _.filter(events, function (event) {
-        return event.end >= new Date();
-      });
+      /**
+      * Get and store new token after prompting for user authorization, and then
+      * execute the given callback with the authorized OAuth2 client.
+      *
+      * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+      * @param {getEventsCallback} callback The callback to call with the authorized
+      *     client.
+      */
+      function getNewToken(oauth2Client, callback) {
+        var authUrl = oauth2Client.generateAuthUrl({
+          access_type: 'offline',
+          scope: SCOPES
+        });
+        dependencies.logger.info('Authorize this app by visiting this url: ', authUrl);
+        var rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
+        rl.question('Enter the code from that page here: ', function(code) {
+          rl.close();
+          oauth2Client.getToken(code, function(err, token) {
+            if (err) {
+              jobCallback('Error while trying to retrieve access token' + err);
+              return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+          });
+        });
+      }
 
-      var result = [];
-      _.first(events, maxEntries).forEach(function (event) {
-          result.push({startDate: formatDate(event.start), endDate: formatDate(event.end), summary: event.summary});
-      });
+      /**
+      * Store token to disk be used in later program executions.
+      *
+      * @param {Object} token The token to store to disk.
+      */
+      function storeToken(token) {
+        try {
+          fs.mkdirSync(TOKEN_DIR);
+        } catch (err) {
+          if (err.code != 'EEXIST') {
+            throw err;
+          }
+        }
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+        dependencies.logger.info('Token stored to ' + TOKEN_PATH);
+      }
 
-      jobCallback(null, {events: result, title: config.widgetTitle});
-    });
-  }
+      /**
+      * Lists the next 10 events on the user's primary calendar.
+      *
+      * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+      */
+      function listEvents(auth) {
+        var calendar = google.calendar('v3');
+        
+        calendar.events.list({
+          auth: auth,
+          calendarId: 'enjwong@gmail.com',
+          timeMin: (new Date()).toISOString(),
+          maxResults: 10,
+          singleEvents: true,
+          orderBy: 'startTime'
+        }, function(err, response) {
+          if (err) {
+            jobCallback('The API returned an error: ' + err);
+            return;
+          }
+          jobCallback(null, {events: response.items, title: config.widgetTitle});
+        });
+      }
+    }
 };
--- packages/default/widgets/calendar/calendar.js
+++ packages/default/widgets/calendar/calendar.js
@@ -16,7 +16,7 @@ widget = {
 
       data.events.forEach(function (event) {
         var eventDiv = $("<div/>").addClass('leave-event');
-        $(eventDiv).append($("<div/>").addClass('leave-dates').append(event.startDate + " - " + event.endDate));
+        $(eventDiv).append($("<div/>").addClass('leave-dates').append(event.start.dateTime + " - " + event.end.dateTime));
         $(eventDiv).append($("<div/>").addClass('leave-summary').append(event.summary));
 
         $content.append(eventDiv);
