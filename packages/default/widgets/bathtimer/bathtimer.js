53f46fd838e3d43ca5fc5d28f8a54c6c1b147697
Joseph Wong <joseph.wong@D2L.com>
2016-02-09 13:58:00 -0500

    indentation and some code for bathtimer

--- packages/default/widgets/bathtimer/bathtimer.js
+++ packages/default/widgets/bathtimer/bathtimer.js
@@ -1,18 +1,25 @@
 widget = {
 
-  onInit: function( el ) {
+    onInit: function( el ) {
 
-  },
+        $( '#levibath', el ).click( function() {
+            $.get( '/jobs/bathtimer/resetBathTimer' );
+        });
 
-  //runs when we receive data from the job
-  onData: function (el, data) {
+        $( '#leiabath', el ).click( function() {
+            $.get( '/jobs/bathtimer/resetBathTimer' );
+        });
+    },
 
-    //The parameters our job passed through are in the data object
-    //el is our widget element, so our actions should all be relative to that
-    if (data.title) {
-      $('h2', el).text(data.title);
+    //runs when we receive data from the job
+    onData: function (el, data) {
+
+        //The parameters our job passed through are in the data object
+        //el is our widget element, so our actions should all be relative to that
+        if (data.title) {
+            $('h2', el).text(data.title);
+        }
+
+        $('.content', el).html(data.text);
     }
-
-    $('.content', el).html(data.text);
-  }
 };
